22bca1cdd5b05b4a2c907bddbd586c8408e39743
Vanity <ktkyletran@gmail.com>
2021-11-27 21:32:01 -0600

    added auth and navbar components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
+import Auth from './components/Auth/Auth';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Creative Crowd</h1>
+      <Navbar />
+      <Auth />
     </div>
   );
 }
