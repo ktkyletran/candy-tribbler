485042a2d82607f0678ff8b9df1d058aacf78519
Vanity <ktkyletran@gmail.com>
2021-11-29 21:08:36 -0600

    added react router dom and switches

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import Auth from './components/Auth/Auth';
+import Home from './components/Home/Home';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Auth />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
